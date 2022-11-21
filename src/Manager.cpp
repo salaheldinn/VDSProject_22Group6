c5f9b0e4f18e2e4bf7a6e76b244090e576547982
yazan7k <yazan7k@gmail.com>
2022-11-21 22:09:11 +0100

    declaring Manager class + constructor

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -1,1 +1,13 @@
 #include "Manager.h"
+#include <vector>
+#include <unordered_map>
+
+using namespace ClassProject;
+
+Manager::Manager(){
+    //Initialize False and True in the unique table
+    unique_table_entry temp_entry = {.id=0, .high=0, .low=0, .topvar=0, .label="False"};
+    unique_table.push_back(temp_entry);
+    temp_entry = {.id=1, .high=1, .low=1, .topvar=1, .label="True"};
+    unique_table.push_back(temp_entry);
+}
--- src/Manager.h
+++ src/Manager.h
@@ -6,9 +6,70 @@
 #define VDSPROJECT_MANAGER_H
 
 #include "ManagerInterface.h"
+#include <vector>
+
 
 namespace ClassProject {
 
+    struct unique_table_entry {
+        BDD_ID id;
+        BDD_ID high;
+        BDD_ID low;
+        BDD_ID topvar;
+        std::string label;
+    };
+
+    class Manager : public ManagerInterface {
+    public:
+        std::vector<unique_table_entry> unique_table;
+
+        Manager();
+
+        BDD_ID createVar(const std::string &label);
+
+        const BDD_ID &True();
+
+        const BDD_ID &False();
+
+        bool isConstant(BDD_ID f);
+
+        bool isVariable(BDD_ID x);
+
+        BDD_ID topVar(BDD_ID f);
+
+        BDD_ID ite(BDD_ID i, BDD_ID t, BDD_ID e);
+
+        BDD_ID coFactorTrue(BDD_ID f, BDD_ID x);
+
+        BDD_ID coFactorFalse(BDD_ID f, BDD_ID x);
+
+        BDD_ID coFactorTrue(BDD_ID f);
+
+        BDD_ID coFactorFalse(BDD_ID f);
+
+        BDD_ID neg(BDD_ID a);
+
+        BDD_ID and2(BDD_ID a, BDD_ID b);
+
+        BDD_ID or2(BDD_ID a, BDD_ID b);
+
+        BDD_ID xor2(BDD_ID a, BDD_ID b);
+
+        BDD_ID nand2(BDD_ID a, BDD_ID b);
+
+        BDD_ID nor2(BDD_ID a, BDD_ID b);
+
+        BDD_ID xnor2(BDD_ID a, BDD_ID b);
+
+        std::string getTopVarName(const BDD_ID &root);
+
+        void findNodes(const BDD_ID &root, std::set<BDD_ID> &nodes_of_root);
+
+        void findVars(const BDD_ID &root, std::set<BDD_ID> &vars_of_root);
+
+        size_t uniqueTableSize();
+
+    };
 }
 
 #endif
