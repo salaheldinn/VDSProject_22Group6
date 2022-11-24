1a5358e7a98bb62f28dc5c8b2c8ee74d3ee453cd
salaheldinn <salaheldin.m.attia@gmail.com>
2022-11-24 23:57:37 +0100

    Test_Manager_Init_Cunstructor

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -11,6 +11,25 @@ int main(int argc, char* argv[])
     return RUN_ALL_TESTS();
 }
 
+
 TEST(HelloTest, BasicAssertions) {
     EXPECT_EQ(7 * 6, 42);
 }
+
+TEST(ManagerFuncsTest, TestUniqueTableInit) {
+
+    ClassProject:: Manager Test_ROBDD   ;
+
+    EXPECT_EQ(0,Test_ROBDD.unique_table[0].id);
+    EXPECT_EQ(0,Test_ROBDD.unique_table[0].high);
+    EXPECT_EQ(0,Test_ROBDD.unique_table[0].low);
+    EXPECT_EQ(0,Test_ROBDD.unique_table[0].topvar);
+    EXPECT_EQ("False",Test_ROBDD.unique_table[0].label);
+
+    EXPECT_EQ(1,Test_ROBDD.unique_table[1].id);
+    EXPECT_EQ(1,Test_ROBDD.unique_table[1].high);
+    EXPECT_EQ(1,Test_ROBDD.unique_table[1].low);
+    EXPECT_EQ(1,Test_ROBDD.unique_table[1].topvar);
+    EXPECT_EQ("True",Test_ROBDD.unique_table[1].label);
+}
+
