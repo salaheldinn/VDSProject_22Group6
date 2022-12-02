dc6aaacb7c82ca077ca224b48a4c0fefbcff1a2e
yazan7k <yazan7k@gmail.com>
2022-12-02 16:39:55 +0100

    implementation of functions ite, coFactorTrue and CoFactorFalse

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -53,11 +53,67 @@ BDD_ID Manager::find_or_add_unique_table(BDD_ID tv, BDD_ID h, BDD_ID l){
     //entry found
     else return indx->second;
 }
-BDD_ID Manager::ite(BDD_ID i, BDD_ID t, BDD_ID e){}
 
-BDD_ID Manager::coFactorTrue(BDD_ID f, BDD_ID x){};
+BDD_ID Manager::ite(BDD_ID i, BDD_ID t, BDD_ID e){
+    //check for terminal cases
+    if (i==0)
+        return e;
+    if ((i==1) || (t==e))
+        return t;
+    //not a terminal case
+    //top variable added first has higher priority
+    BDD_ID topvar_x;
+    if (isConstant(t) && isConstant(e))
+        topvar_x = topVar(i);
+    else if (isConstant(t))
+        topvar_x = std::min(topVar(i), topVar(e));
+    else if (isConstant(e))
+        topvar_x = std::min(topVar(i), topVar(t));
+    else
+        topvar_x = std::min(std::min(topVar(i), topVar(t)), topVar(e));
+    //calculate positive and negative cofactors
+    BDD_ID i_pos_cofactor = coFactorTrue(i, topvar_x);
+    BDD_ID i_neg_cofactor = coFactorFalse(i, topvar_x);
+    BDD_ID t_pos_cofactor = coFactorTrue(t, topvar_x);
+    BDD_ID t_neg_cofactor = coFactorFalse(t, topvar_x);
+    BDD_ID e_pos_cofactor = coFactorTrue(e, topvar_x);
+    BDD_ID e_neg_cofactor = coFactorFalse(e, topvar_x);
+    //calculate HIGH and LOW
+    BDD_ID r_high = ite(i_pos_cofactor, t_pos_cofactor, e_pos_cofactor);
+    BDD_ID r_low = ite(i_neg_cofactor, t_neg_cofactor, e_neg_cofactor);
+    //reduce if possible
+    if (r_high==r_low)
+        return r_high;
+    //add to unique table if it does not exist already
+    BDD_ID r = find_or_add_unique_table(topvar_x, r_high, r_low);
+    return r;
+}
 
-BDD_ID Manager::coFactorFalse(BDD_ID f, BDD_ID x){};
+BDD_ID Manager::coFactorTrue(BDD_ID f, BDD_ID x){
+    //check for terminal case
+    if (isConstant(f) || isConstant(x) || topVar(f)>x)
+        return f;
+    //not a terminal case
+    if (topVar(f) == x)
+        return coFactorTrue(f);
+    //recursion
+    BDD_ID t_temp = coFactorTrue(coFactorTrue(f), x);
+    BDD_ID f_temp = coFactorTrue(coFactorFalse(f), x);
+    return ite(topVar(f), t_temp, f_temp);
+}
+
+BDD_ID Manager::coFactorFalse(BDD_ID f, BDD_ID x){
+    //check for terminal case
+    if (isConstant(f) || isConstant(x) || topVar(f)>x)
+        return f;
+    //not a terminal case
+    if (topVar(f) == x)
+        return coFactorFalse(f);
+    //recursion
+    BDD_ID t_temp = coFactorFalse(coFactorTrue(f), x);
+    BDD_ID f_temp = coFactorFalse(coFactorFalse(f), x);
+    return ite(topVar(f), t_temp, f_temp);
+}
 
 BDD_ID Manager::coFactorTrue(BDD_ID f){
     return unique_table[f].high;
