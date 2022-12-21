5d46d53c77e0d9f78fb2e16bce5312a1a042a4b4
yazan7k <yazan7k@gmail.com>
2022-12-21 19:53:54 +0100

    override virtual functions

--- src/Manager.h
+++ src/Manager.h
@@ -54,51 +54,51 @@ namespace ClassProject {
 
         Manager();
 
-        BDD_ID createVar(const std::string &label);
+        BDD_ID createVar(const std::string &label) override;
 
-        const BDD_ID &True();
+        const BDD_ID &True() override;
 
-        const BDD_ID &False();
+        const BDD_ID &False() override;
 
-        bool isConstant(BDD_ID f);
+        bool isConstant(BDD_ID f) override;
 
-        bool isVariable(BDD_ID x);
+        bool isVariable(BDD_ID x) override;
 
-        BDD_ID topVar(BDD_ID f);
+        BDD_ID topVar(BDD_ID f) override;
 
         BDD_ID find_or_add_unique_table(BDD_ID tv, BDD_ID h, BDD_ID l);
 
-        BDD_ID ite(BDD_ID i, BDD_ID t, BDD_ID e);
+        BDD_ID ite(BDD_ID i, BDD_ID t, BDD_ID e) override;
 
-        BDD_ID coFactorTrue(BDD_ID f, BDD_ID x);
+        BDD_ID coFactorTrue(BDD_ID f, BDD_ID x) override;
 
-        BDD_ID coFactorFalse(BDD_ID f, BDD_ID x);
+        BDD_ID coFactorFalse(BDD_ID f, BDD_ID x) override;
 
-        BDD_ID coFactorTrue(BDD_ID f);
+        BDD_ID coFactorTrue(BDD_ID f) override;
 
-        BDD_ID coFactorFalse(BDD_ID f);
+        BDD_ID coFactorFalse(BDD_ID f) override;
 
-        BDD_ID neg(BDD_ID a);
+        BDD_ID neg(BDD_ID a) override;
 
-        BDD_ID and2(BDD_ID a, BDD_ID b);
+        BDD_ID and2(BDD_ID a, BDD_ID b) override;
 
-        BDD_ID or2(BDD_ID a, BDD_ID b);
+        BDD_ID or2(BDD_ID a, BDD_ID b) override;
 
-        BDD_ID xor2(BDD_ID a, BDD_ID b);
+        BDD_ID xor2(BDD_ID a, BDD_ID b) override;
 
-        BDD_ID nand2(BDD_ID a, BDD_ID b);
+        BDD_ID nand2(BDD_ID a, BDD_ID b) override;
 
-        BDD_ID nor2(BDD_ID a, BDD_ID b);
+        BDD_ID nor2(BDD_ID a, BDD_ID b) override;
 
-        BDD_ID xnor2(BDD_ID a, BDD_ID b);
+        BDD_ID xnor2(BDD_ID a, BDD_ID b) override;
 
-        std::string getTopVarName(const BDD_ID &root);
+        std::string getTopVarName(const BDD_ID &root) override;
 
-        void findNodes(const BDD_ID &root, std::set<BDD_ID> &nodes_of_root);
+        void findNodes(const BDD_ID &root, std::set<BDD_ID> &nodes_of_root) override;
 
-        void findVars(const BDD_ID &root, std::set<BDD_ID> &vars_of_root);
+        void findVars(const BDD_ID &root, std::set<BDD_ID> &vars_of_root) override;
 
-        size_t uniqueTableSize();
+        size_t uniqueTableSize() override;
 
     };
 }
