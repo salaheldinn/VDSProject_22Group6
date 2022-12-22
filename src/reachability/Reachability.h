006b77475e3d03ac46f4bee793769e3dc6e4ac9f
yazan7k <yazan7k@gmail.com>
2022-12-22 11:49:18 +0100

    declare Reachability class in header

--- src/reachability/Reachability.h
+++ src/reachability/Reachability.h
@@ -5,9 +5,36 @@
 
 namespace ClassProject {
 
-//    class Reachability : public ReachabilityInterface {
-//
-//    };
+    class Reachability : public ReachabilityInterface {
+    public:
+        std::vector<BDD_ID> state_variables;
+
+        std::vector<BDD_ID> next_state_variables;
+
+        std::vector<BDD_ID> transition_functions;
+
+        std::vector<bool> init_state; //initial state
+
+        BDD_ID existential_quantification(BDD_ID f, BDD_ID x);
+
+        BDD_ID img(BDD_ID f); //img(s0, s1)
+
+        BDD_ID img_nxt(BDD_ID f); //img(s0', s1')
+
+        BDD_ID translation_relation(); //Tau
+
+        BDD_ID bool_to_BDD_ID(const std::vector<bool> &stateVector); //adds state to ROBDD
+
+        explicit Reachability(unsigned int stateSize);
+
+        const std::vector<BDD_ID> &getStates() const override;
+
+        bool isReachable(const std::vector<bool> &stateVector) override;
+
+        void setTransitionFunctions(const std::vector<BDD_ID> &transitionFunctions) override;
+
+        void setInitState(const std::vector<bool> &stateVector) override;
+    };
 
 }
 #endif
