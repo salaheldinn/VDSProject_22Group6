6e787c89e3b9a744f2a24ae48d7e2e055506febd
yazan7k <yazan7k@gmail.com>
2023-01-07 11:34:29 +0100

    remove accesses to arrays in tests of manager class

--- src/test/main_test.cpp
+++ src/test/main_test.cpp
@@ -17,239 +17,127 @@ TEST(HelloTest, BasicAssertions) {
 }
 /*Test the Constructor Manager*/
 TEST(ManagerFuncsTest, TestUniqueTableInit) {
-
     ClassProject:: Manager Test_ROBDD   ;
+    EXPECT_EQ(0,Test_ROBDD.False());
+    EXPECT_EQ(0,Test_ROBDD.coFactorTrue(0));
+    EXPECT_EQ(0,Test_ROBDD.coFactorTrue(0));
+    EXPECT_EQ(0,Test_ROBDD.topVar(0));
 
-    EXPECT_EQ(0,Test_ROBDD.unique_table[0].id);
-    EXPECT_EQ(0,Test_ROBDD.unique_table[0].high);
-    EXPECT_EQ(0,Test_ROBDD.unique_table[0].low);
-    EXPECT_EQ(0,Test_ROBDD.unique_table[0].topvar);
-    EXPECT_EQ("False",Test_ROBDD.unique_table[0].label);
+    EXPECT_EQ(1,Test_ROBDD.True());
+    EXPECT_EQ(1,Test_ROBDD.coFactorTrue(1));
+    EXPECT_EQ(1,Test_ROBDD.coFactorTrue(1));
+    EXPECT_EQ(1,Test_ROBDD.topVar(1));
 
-    EXPECT_EQ(1,Test_ROBDD.unique_table[1].id);
-    EXPECT_EQ(1,Test_ROBDD.unique_table[1].high);
-    EXPECT_EQ(1,Test_ROBDD.unique_table[1].low);
-    EXPECT_EQ(1,Test_ROBDD.unique_table[1].topvar);
-    EXPECT_EQ("True",Test_ROBDD.unique_table[1].label);
+    EXPECT_EQ(2,Test_ROBDD.uniqueTableSize());
+
 }
 
 /*Test Function "BDD_ID Manager::createVar(const std::string &label){};"*/
 TEST(ManagerFuncsTest, TestCreateVar) {
 
     ClassProject:: Manager Test_ROBDD   ;
-    ClassProject:: BDD_ID Test_ID;
-
-    for (int i = 2 ; i < 10 ; i++)
-    {
-
-        Test_ID = Test_ROBDD.createVar("Test");
-
-        EXPECT_EQ(Test_ID,Test_ROBDD.unique_table[i].id);
-        EXPECT_EQ("Test",Test_ROBDD.unique_table[i].label);
-        EXPECT_EQ(1,Test_ROBDD.unique_table[i].high);
-        EXPECT_EQ(0,Test_ROBDD.unique_table[i].low);
-        EXPECT_EQ(i,Test_ROBDD.unique_table[i].topvar);
-
-    }
-
+    ClassProject:: BDD_ID a = Test_ROBDD.createVar("a");
+    EXPECT_EQ(a,Test_ROBDD.topVar(a));
+    EXPECT_EQ(1,Test_ROBDD.coFactorTrue(a));
+    EXPECT_EQ(0,Test_ROBDD.coFactorFalse(a));
+    EXPECT_EQ("a",Test_ROBDD.getTopVarName(a));
+    EXPECT_EQ(3,Test_ROBDD.uniqueTableSize());
 }
-
-
 
 /*Test Function "const BDD_ID &Manager::True(){}"*/
 TEST(ManagerFuncsTest, TestTrueFunc) {
-
     ClassProject:: Manager Test_ROBDD   ;
     EXPECT_EQ(1,Test_ROBDD.True());
 }
 
 /*Test Function "const BDD_ID &Manager::False(){}"*/
 TEST(ManagerFuncsTest, TestFalseFunc) {
-
     ClassProject:: Manager Test_ROBDD   ;
     EXPECT_EQ(0,Test_ROBDD.False());
 }
 
-
-
 /*Test Function "bool Manager::isConstant(BDD_ID f){};*/
 TEST(ManagerFuncsTest, Test_isConstantFunc) {
-
-    /*Declare used variables*/
     ClassProject::Manager Test_ROBDD;
-    bool Loc_isLeafNode;
-
-    /**/
-    Test_ROBDD.createVar("a");
-
-    for (ClassProject::BDD_ID i = 0 ; i < 3 ; i++)
-    {
-
-        Loc_isLeafNode = Test_ROBDD.isConstant(i);
-        if (i > 1)
-        {
-            EXPECT_FALSE(Loc_isLeafNode);
-        }
-        else
-        {
-            EXPECT_TRUE(Loc_isLeafNode);
-        } //end if
-
-    } //end for
-} //end func
+    ClassProject:: BDD_ID a = Test_ROBDD.createVar("a");
+    EXPECT_FALSE(Test_ROBDD.isConstant(a));
+    EXPECT_TRUE(Test_ROBDD.isConstant(Test_ROBDD.True()));
+    EXPECT_TRUE(Test_ROBDD.isConstant(Test_ROBDD.False()));
+}
 
 /*Test Function "bool Manager::isVariable(BDD_ID x){};"*/
 TEST(ManagerFuncsTest, Test_isVarFunc) {
-
-    /*Declare used variables*/
     ClassProject::Manager Test_ROBDD;
-    bool Loc_isVarNode;
-
-    /**/
-    Test_ROBDD.createVar("a");
-
-    for (ClassProject::BDD_ID i = 0 ; i < 3 ; i++)
-    {
-
-        Loc_isVarNode = Test_ROBDD.isVariable(i);
-        /*this if statement need to be updated after implementing and2, or2, ... */
-        if (i <= 1)
-        {
-            EXPECT_FALSE(Loc_isVarNode);
-        }
-        else
-        {
-            EXPECT_TRUE(Loc_isVarNode);
-        } //end if
-
-    } //end for
-} //end func
-
-
+    ClassProject:: BDD_ID a = Test_ROBDD.createVar("a");
+    EXPECT_TRUE(Test_ROBDD.isVariable(a));
+    EXPECT_FALSE(Test_ROBDD.isVariable(Test_ROBDD.True()));
+    EXPECT_FALSE(Test_ROBDD.isVariable(Test_ROBDD.False()));
+}
 
 /*Test Function "BDD_ID Manager::topVar(BDD_ID f){};"*/
 TEST(ManagerFuncsTest, Test_topVarFunc) {
-
-    /*Declare used variables*/
     ClassProject::Manager Test_ROBDD;
-
-    /**/
-    Test_ROBDD.createVar("a");
-
-    for (ClassProject::BDD_ID i = 0 ; i < 3 ; i++)
-    {
-        EXPECT_EQ(Test_ROBDD.unique_table[i].topvar, Test_ROBDD.topVar(i));
-    }
-
-
+    ClassProject:: BDD_ID a = Test_ROBDD.createVar("a");
+    EXPECT_EQ(a, Test_ROBDD.topVar(a));
+    EXPECT_EQ(0, Test_ROBDD.topVar(Test_ROBDD.False()));
+    EXPECT_EQ(1, Test_ROBDD.topVar(Test_ROBDD.True()));
 }
-
-TEST(ManagerFuncsTest, Test_hash_unique_table) {
-
-    ClassProject::Manager Test_ROBDD;
-    ClassProject::BDD_ID id_of_a = Test_ROBDD.createVar("a");
-    //find entry of True
-    auto indx = Test_ROBDD.hash_unique_table.find({1, 1, 1});
-    EXPECT_EQ(indx->second, 1);
-    //find entry of a
-    indx = Test_ROBDD.hash_unique_table.find({1, 0, 2});
-    EXPECT_EQ(indx->second, id_of_a);
-    //find non-existent entry
-    indx = Test_ROBDD.hash_unique_table.find({1, 0, 3});
-    EXPECT_EQ(indx, Test_ROBDD.hash_unique_table.end());
-}
-
-
 
 /*Test Function "size_t Manager::uniqueTableSize(){};"*/
-/*this test will be improved after implementing and2, or2...functions*/
 TEST(ManagerFuncsTest, Test_uniqueTableSize) {
-
-    /*Declare used variables*/
     ClassProject::Manager Test_ROBDD;
-
-    Test_ROBDD.createVar("a");
-    Test_ROBDD.createVar("b");
+    EXPECT_EQ(2, Test_ROBDD.uniqueTableSize());
+    ClassProject:: BDD_ID a = Test_ROBDD.createVar("a");
+    ClassProject:: BDD_ID b = Test_ROBDD.createVar("b");
     EXPECT_EQ(4, Test_ROBDD.uniqueTableSize());
+    Test_ROBDD.and2(a, b);
+    EXPECT_EQ(5, Test_ROBDD.uniqueTableSize());
 }
 
-
 /*Test Function "BDD_ID Manager::coFactorTrue(BDD_ID f){};"*/
-/*this test will be improved after implementing and2, or2...functions*/
 TEST(ManagerFuncsTest, Test_coFactorTrue1) {
-
-    /*Declare used variables*/
     ClassProject::Manager Test_ROBDD;
-
-    /**/
-    Test_ROBDD.createVar("a");
-
-    for (ClassProject::BDD_ID i = 0 ; i < 3 ; i++)
-    {
-        EXPECT_EQ(Test_ROBDD.unique_table[i].high, Test_ROBDD.coFactorTrue(i));
-    }
+    ClassProject:: BDD_ID a = Test_ROBDD.createVar("a");
+    ClassProject:: BDD_ID b = Test_ROBDD.createVar("b");
+    ClassProject:: BDD_ID a_and_b = Test_ROBDD.and2(a, b);
+    EXPECT_EQ(1, Test_ROBDD.coFactorTrue(a));
+    EXPECT_EQ(b, Test_ROBDD.coFactorTrue(a_and_b));
 }
 
 /*Test Function "BDD_ID Manager::coFactorFalse(BDD_ID f){};"*/
-/*this test will be improved after implementing and2, or2...functions*/
 TEST(ManagerFuncsTest, Test_coFactorFalse1) {
-
-    /*Declare used variables*/
     ClassProject::Manager Test_ROBDD;
-
-    /**/
-    Test_ROBDD.createVar("a");
-
-    for (ClassProject::BDD_ID i = 0 ; i < 3 ; i++)
-    {
-        EXPECT_EQ(Test_ROBDD.unique_table[i].low, Test_ROBDD.coFactorFalse(i));
-    }
+    ClassProject:: BDD_ID a = Test_ROBDD.createVar("a");
+    ClassProject:: BDD_ID b = Test_ROBDD.createVar("b");
+    ClassProject:: BDD_ID a_or_b = Test_ROBDD.or2(a, b);
+    EXPECT_EQ(0, Test_ROBDD.coFactorFalse(a));
+    EXPECT_EQ(b, Test_ROBDD.coFactorFalse(a_or_b));
 }
 
 TEST(ManagerFuncsTest, Test_find_or_add_unique_table) {
     ClassProject::Manager Test_ROBDD;
-    ClassProject::BDD_ID id_of_a = Test_ROBDD.createVar("a");
-    //find ID of a
-    EXPECT_EQ(Test_ROBDD.find_or_add_unique_table(2, 1, 0), id_of_a);
-    //add new entry
-    EXPECT_EQ(Test_ROBDD.find_or_add_unique_table(3, 1, 0), 3);
-}
-
-
-/*Test Function "BDD_ID Manager::ite(BDD_ID i, BDD_ID t, BDD_ID e){}"*/
-/*this test will be improved after implementing and2, or2...functions*/
-TEST(ManagerFuncsTest, Test_ite) {
-
-    /*Declare used variables*/
-    ClassProject::Manager Test_ROBDD;
-
-    /**/
     ClassProject::BDD_ID a = Test_ROBDD.createVar("a");
     ClassProject::BDD_ID b = Test_ROBDD.createVar("b");
+    //find ID of a and b
+    EXPECT_EQ(Test_ROBDD.find_or_add_unique_table(a, 1, 0), a);
+    EXPECT_EQ(Test_ROBDD.find_or_add_unique_table(b, 1, 0), b);
+    //add new entry
+    EXPECT_EQ(Test_ROBDD.find_or_add_unique_table(a, b, 0), 4);
+}
 
-
-    /*All terminal cases*/
-    ClassProject::BDD_ID  exp_a= Test_ROBDD.ite(1,a,b);
-    ClassProject::BDD_ID  exp_b= Test_ROBDD.ite(0,a,b);
-    ClassProject::BDD_ID  exp_a2= Test_ROBDD.ite(a,1,0);
-    ClassProject::BDD_ID  exp_b2= Test_ROBDD.ite(a,b,b);
-    ClassProject::BDD_ID  exp_notA= Test_ROBDD.ite(a,0,1);
-    ClassProject::BDD_ID  exp_a3 = Test_ROBDD.ite(exp_notA,0,1); //to test computed table, "a" should not be repeated in unique table
-
-
+/*Test Function "BDD_ID Manager::ite(BDD_ID i, BDD_ID t, BDD_ID e){}"*/
+TEST(ManagerFuncsTest, Test_ite) {
+    ClassProject::Manager Test_ROBDD;
+    ClassProject::BDD_ID a = Test_ROBDD.createVar("a");
+    ClassProject::BDD_ID b = Test_ROBDD.createVar("b");
     /*Terminal Cases test*/
-    EXPECT_EQ(2, exp_a);
-    EXPECT_EQ(2, exp_a2);
-    EXPECT_EQ(2, exp_a3);
+    EXPECT_EQ(a, Test_ROBDD.ite(1,a,b));
+    EXPECT_EQ(a, Test_ROBDD.ite(a,1,0));
+    EXPECT_EQ(b, Test_ROBDD.ite(0,a,b));
+    EXPECT_EQ(b, Test_ROBDD.ite(a,b,b));
 
-    EXPECT_EQ(3, exp_b);
-    EXPECT_EQ(3, exp_b2);
-
-    EXPECT_EQ(4, exp_notA);
-    EXPECT_EQ(2, Test_ROBDD.unique_table[exp_notA].topvar);
-    EXPECT_EQ(0, Test_ROBDD.unique_table[exp_notA].high);
-    EXPECT_EQ(1, Test_ROBDD.unique_table[exp_notA].low);
-
+    ClassProject::BDD_ID a_and_b = Test_ROBDD.and2(a, b);
+    EXPECT_EQ(a_and_b, Test_ROBDD.ite(a,b,0));
 }
 
 TEST(ManagerFuncsTest, Test_getTopVarName) {
